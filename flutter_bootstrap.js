91b1911599e4edfb5c9eab9c06fb11ddbadb844b
RhiPlayGames <rhisayce@gmail.com>
2026-09-06 22:28:47 +0100

    Deploy latest First Guess web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3613326859" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2475594491" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
